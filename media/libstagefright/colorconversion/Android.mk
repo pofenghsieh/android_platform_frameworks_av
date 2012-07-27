d115371e606bf6d63256120c6c7bc281c7d07d3b
Devaraj Rangasamy <dev@ti.com>
2012-07-26 19:45:27 -0500

    Stagefright: Use update TI OMX Headers
    Since domx in omap4xxx tree is updated, use the definitions
    in there instead of defining them in stagefright.
    
    Change-Id: Iecf014ce349e68110fa7343f620233f4e2aec4d8
    Signed-off-by: Tyler Luu <tluu@ti.com>
    Signed-off-by: Devaraj Rangasamy <dev@ti.com>

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -11,4 +11,12 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_MODULE:= libstagefright_color_conversion
 
+ifeq ($(OMAP_ENHANCEMENT), true)
+ifeq ($(ENHANCED_DOMX), true)
+LOCAL_C_INCLUDES += $(TOP)/hardware/ti/domx/omx_core/inc
+else
+LOCAL_C_INCLUDES += $(TOP)/hardware/ti/omap4xxx/domx/omx_core/inc
+endif
+endif
+
 include $(BUILD_STATIC_LIBRARY)
